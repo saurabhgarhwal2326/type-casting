097b88b1de8e4cde8dbf9085a706076954b990db
saurabh garhwal <saurabhgarhwal353@gmail.com>
2024-07-09 23:42:04 +0530

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,80 @@
+print("hello world")
+num1=input('plz enter first number')
+num2=input("plz inter second number")
+print(num1+num2)
+
+type.casting
+int(),float(),str(),dict(),list(),tuple(),set()
+
+var=10
+
+var= int(var)
+print(var)
+print(type (var))
+
+int(), float()
+
+
+num1=int(input('plz enter first number'))
+num2= int (input("plz inter second number"))
+print(num1)
+print(num2)
+print(type(num1))
+print(type(num2))
+
+
+print(num1//num2)
+print(num1 ** num2)
+
+print(num1 % num2)
+num=10
+num+=5
+print(num)
+
+x=15
+x%=3
+print (x)
+
+num1=int(input('plz enter first number'))
+num2= int (input("plz inter second number"))
+
+print(num1==num2) #equal 
+print(num1!=num2) #
+
+print(num1<=num2)
+print(num1>num2)
+
+
+# >>>>>>>>>conditional statements<<<<<<<<<<p
+
+relative= input("are you relative person of my family press y: ")
+if relative =='yes':  #true
+    print('welcome')
+
+elif relative=="padosi":
+    print("no nees to welcome")
+else:                #false
+    print("bheed kam ")
+
+
+
+    #program
+
+guest_list= ['mohit',"rohit","sunil"]
+Name= input("plese your name :")
+if Name not in guest_list:
+ print("wel come")
+else:
+   print("bheed kam")
+
+
+
+   # test program
+    
+guest_list=['saurabh','kiran','sunil','himanshu','deepak','rajveer']
+Name=input('plese your name:')
+if Name  in guest_list:
+     print ('welcome')
+else:
+      print('bheed kam')
+    
